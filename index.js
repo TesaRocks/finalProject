e26e242425ab7a9f25c78d1224e980ad3870bb33
Alexis Dietl <alexisdietl@gmail.com>
2021-05-10 10:25:43 -0300

    FP-0002-finished

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app = express();
 app.get("/", function (req, res) {
-    res.status(200).send("<h1>Hello World!!!!</h1>");
+    res.status(200).send("<h1>Hello World!!!! I am alexis Dietl</h1>");
 });
 app.listen(8008, function () {
     console.log("server is listening on port 8008");
--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import express = require("express");
 const app: express.Application = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send("<h1>Hello World!!!!</h1>");
+  res.status(200).send("<h1>Hello World!!!! I am alexis Dietl</h1>");
 });
 
 app.listen(8008, () => {
