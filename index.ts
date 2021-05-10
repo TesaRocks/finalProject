31b95d3c0424d3577f794f1c62b9d9ca1a9f32f0
Alexis Dietl <alexisdietl@gmail.com>
2021-05-10 09:36:52 -0300

    FP-0002-installing ts node

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
-const express = require('express');
-const app = express()
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Hello World</h1>");
+});
 
-app.get('/', (req, res)=>{
-res.status(200).send('<h1>Hello World</h1>')
-})
-
-app.listen(8008, ()=>{
-    console.log("server is listening on port 8008")
-})
+app.listen(8008, () => {
+  console.log("server is listening on port 8008");
+});
