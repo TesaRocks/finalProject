34332ee95dc49672dde7ff62c85cfd3938b6b62b
Diego Laciar <diegolaciar@gmail.com>
2021-05-11 11:55:30 -0300

    PF - diego updates

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,37 @@
 import express = require("express");
+const bp = require('body-parser')
 
 const app: express.Application = express();
 
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
+interface IUser {
+  name: string;
+}
+
+const users: IUser[] = [];
+
 app.get("/", (req, res) => {
-  res.status(200).send("Hello World");
+  console.log(req.headers);
+  res.status(200).json(req.headers);
+});
+
+app.post("/user", (req, res) => {  
+  users.push(req.body);
+
+  console.log(req);
+  const obj = {
+    status: "OK"
+  }
+  res.status(200).json(req.body);
+});
+
+app.get("/user", (req, res) => {
+  res.status(200).json(users);
+});
+
+app.get("/user/:id", (req, res) => {
+  res.status(200).json(users[0]);
 });
 
 app.listen(8008, () => {
