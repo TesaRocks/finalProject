82865eb1552c0d1419085dabea54ccc19791611c
Alexis Dietl <alexisdietl@gmail.com>
2021-05-12 14:35:22 -0300

    FP-004-updating tricks.md

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import express from "express";
 import { userRouter } from "./user.router";
 
-const bp = require("body-parser");
+const mysql = require("mysql");
 
+const connection = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "user",
+  password: "password",
+  database: "db",
+});
+connection.connect((err: any) => {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+const bp = require("body-parser");
 const app: express.Application = express();
 
 app.use(bp.json());
@@ -11,16 +23,4 @@ app.use("/user", userRouter);
 
 app.listen(8008, () => {
   console.log("server is listening on port 8008");
-});
-
-const mysql = require("mysql");
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "user",
-  password: "password",
-  database: "db",
-});
-connection.connect((err: any) => {
-  if (err) throw err;
-  console.log("Connected!");
 });
