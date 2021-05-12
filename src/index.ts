e44cc39574e92a06789b9a6851f14dfb5e09dc7d
Alexis Dietl <alexisdietl@gmail.com>
2021-05-12 13:38:51 -0300

    FP-004-creating sql connection

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,15 @@ app.use("/user", userRouter);
 app.listen(8008, () => {
   console.log("server is listening on port 8008");
 });
+
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "user",
+  password: "password",
+  database: "db",
+});
+connection.connect((err: any) => {
+  if (err) throw err;
+  console.log("Connected!");
+});
